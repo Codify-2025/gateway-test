498e8095aae2861dc66b1680f35c715f4ec86de4
dylee00 <dylee6820@gmail.com>
2025-04-06 02:02:59 +0900

    Chore:프로젝트 초기 개발 환경 세팅

---
+++ src/main/java/Codify/gateway_test/GatewayTestApplication.java
@@ -0,0 +1,22 @@
+package Codify.gateway_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class GatewayTestApplication {
+
+	@RequestMapping("/test")
+	public String home() {
+		return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Codify/gateway_test/GatewayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Codify.gateway_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
